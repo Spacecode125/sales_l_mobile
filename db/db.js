ac083b422eadc08f62b2317d257b3a335491db02
khaled bouajila <khaledbouajila5481@gmail.com>
2023-05-13 17:53:18 +0100

    add user auth

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const localDB = "";
+const localDB = "mongodb+srv://khaledbouajila5481:khaled99@cluster0.ymwoyla.mongodb.net/";
 const connectDB = async () => {
   await mongoose.connect(localDB, {
     useNewUrlParser: true,
---
+++ models/user.js
@@ -0,0 +1,40 @@
+const Mongoose = require("mongoose");
+const UserSchema = new Mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user", "salesman"],
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = Mongoose.model("User", UserSchema);
+module.exports = User;
