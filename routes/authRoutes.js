6cd25179533282d4309490275096d1a601c4a7a1
khaled bouajila <khaledbouajila5481@gmail.com>
2023-05-25 18:05:29 +0100

    update password function

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,18 +4,15 @@ const {
   login,
   getUsers,
   getUserById,
-  findEmail,
   updateUser,
   deleteUser,
-  resetPassword,
+  updatePassword
 } = require("../controllers/authController");
 const  {roleAuth}  = require("../middleware/auth");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/", roleAuth(["admin"]), getUsers);
-router.get("/findEmail", findEmail);
-router.route("/resetPass").post(resetPassword);
 router.get("/:userId", roleAuth(["admin","user","salesman"]), getUserById);
 router.put("/update", roleAuth(["admin","user","salesman"]), updateUser);
 router.put("/updatePass", roleAuth(["admin","user","salesman"]), updatePassword);
